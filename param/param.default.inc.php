7be0d88b1e443b5ee2bb0ea33d48aca5fbaf25b5
Eric Quinton <eric.quinton@irstea.fr>
2014-02-24 17:37:47 +0100

    Integration du droit consult, début d'organisation de la structure des groupes d'utilisateurs

--- param/param.default.inc.php
+++ param/param.default.inc.php
@@ -103,7 +103,7 @@ $GACL_dbprefixe = "gacl";
 $GACL_aro = "login";
 $GACL_aco = "appli";
 $GACL_path = "param/gacl.ini.php";
-$GACL_listeDroitsGeres = "admin,gestion";
+$GACL_listeDroitsGeres = "admin,consult";
 /*
  * Gestion des erreurs
  */
