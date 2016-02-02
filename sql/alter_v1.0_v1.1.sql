518e3616ab875381b356726f4ec499fb52971c2e
Eric Quinton <eric.quinton@irstea.fr>
2016-02-02 10:06:30 +0100

    Suppression des modules tiers non utilisés

--- sql/alter_v1.0_v1.1.sql
+++
@@ -1,2 +0,0 @@
-alter table distribution add column distribution_jour_soir varchar;
-comment on column distribution.distribution_jour_soir is 'Distribution exclusivement le soir d''une demi-ration';
--- sql/alter_v1.1_v1.1.1.sql
+++
@@ -1,2 +0,0 @@
-alter table poisson add column commentaire varchar null ;
-comment on column poisson.commentaire is 'Commentaire général concernant le poisson';
