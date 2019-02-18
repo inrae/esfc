78f5221a0ba79dbe105c3316ca2e1ffaeaeba7cc
Eric Quinton <eric.quinton@irstea.fr>
2019-02-18 12:29:59 +0100

    :feat deploiement: début de mise au point des scripts de diffusion

--- install/alter-1.6.5-1.6.6.sql
+++
@@ -1,6 +0,0 @@
-/*
- * ajout de la saturation en oxygene dans les analyses d'eau
- */
- alter table analyse_eau add column o2_pc float;
- comment on column analyse_eau.o2_pc is 'Oxygène dissous, en pourcentage de saturation';
- 
--- install/update_v1.6.2.sql
+++
@@ -1,4 +0,0 @@
-
-alter table ventilation alter battement_nb type float;
-
-comment on column ventilation.battement_nb is 'Nombre de battements/seconde';
