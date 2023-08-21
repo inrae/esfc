a0f00e0c7b6fd4630f92574bace46fbd4e7910df
Eric Quinton <eric.quinton@inrae.fr>
2023-08-21 16:17:16 +0200

    :rel 2.0.1: préparation de la version

--- locales/C/LC_MESSAGES/add_us_translation.sh
+++ locales/C/LC_MESSAGES/add_us_translation.sh
@@ -1,3 +1,3 @@
-# cp en.po us.po
+cp en.po us.po
 poedit us.po
 msgfmt us.po -o us.mo
--- param/param.default.inc.php
+++ param/param.default.inc.php
@@ -7,9 +7,9 @@
  * Parametres par defaut de l'application
  * Si des modifications doivent etre apportees, faites-les dans le fichier param.inc.php
  */
-$APPLI_version = "2.0.0";
+$APPLI_version = "2.0.1";
 $APPLI_dbversion = "2.0";
-$APPLI_versiondate = _("10/08/2023");
+$APPLI_versiondate = _("21/08/2023");
 $language = "fr";
 $DEFAULT_formatdate = "fr";
 /*
